a352296b1feeac399a5edd74ec1c1c32ad1d027f
shibme <shiblym@shibme-mac.local>
2018-03-08 10:48:00 +0530

    making certificate trust method static

--- src/me/shib/java/lib/restiny/RESTinyClient.java
+++ src/me/shib/java/lib/restiny/RESTinyClient.java
@@ -14,6 +14,8 @@ import java.security.cert.X509Certificate;
  */
 public final class RESTinyClient {
 
+    private static boolean trustingAllCertificates = false;
+
     private String endPoint;
     private JsonUtil jsonUtil;
 
@@ -23,50 +25,35 @@ public final class RESTinyClient {
      * @param endPoint The base URI of the service where the API is hosted
      */
     public RESTinyClient(String endPoint) {
-        new RESTinyClient(endPoint, false);
-    }
-
-    public RESTinyClient(String endPoint, boolean trustAllCertificates) {
         this.endPoint = endPoint;
         this.jsonUtil = new JsonUtil();
-        if(trustAllCertificates) {
-            try {
-                trustAllCerts();
-            } catch (KeyManagementException e) {
-                e.printStackTrace();
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-            }
-        }
     }
 
-    private void trustAllCerts() throws KeyManagementException, NoSuchAlgorithmException {
-        // Create a trust manager that does not validate certificate chains
-        TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
-            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return null;
+    public static synchronized void trustAllCerts() throws KeyManagementException, NoSuchAlgorithmException {
+        if(!trustingAllCertificates) {
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
             }
-            public void checkClientTrusted(X509Certificate[] certs, String authType) {
-            }
-            public void checkServerTrusted(X509Certificate[] certs, String authType) {
-            }
+            };
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+            trustingAllCertificates = true;
         }
-        };
-
-        // Install the all-trusting trust manager
-        SSLContext sc = SSLContext.getInstance("SSL");
-        sc.init(null, trustAllCerts, new java.security.SecureRandom());
-        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
-        // Create all-trusting host name verifier
-        HostnameVerifier allHostsValid = new HostnameVerifier() {
-            public boolean verify(String hostname, SSLSession session) {
-                return true;
-            }
-        };
-
-        // Install the all-trusting host verifier
-        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
     }
 
     /**
